571210c860515cdae4cb7ca8a2152b14cf70a899
adrianomoda <adrianomodafeitosa@gmail.com>
2018-09-29 18:42:50 -0300

    First Commit

---
+++ DecimalToBinary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+    
+    int numDecimal, numBinary, numToOutput, i, k = 0;
+    int binaries[20];
+    int newBinaries[20];
+
+    printf("Entre com um numero inteiro positivo: \n");
+    scanf("%d", &numDecimal);
+    numToOutput = numDecimal;
+
+    for (i = 0; numDecimal > 0; i++) {
+        numBinary = numDecimal % 2;
+        numDecimal = numDecimal / 2;
+        binaries[i] = numBinary;
+	}
+
+    system("cls");    
+    printf("%d to Binary --> ", numToOutput);   
+
+    for(int bin = i-1; bin >= 0; bin--){ 
+        newBinaries[k] = binaries[bin];
+        printf("%d ", newBinaries[k]);
+    }
+
+    return 0;
+}
